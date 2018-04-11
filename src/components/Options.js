a8e3d9d86869ac6f2393cd9d0844ace72f9d6c7f
Paulina <cherry3475@gmail.com>
2018-04-12 01:01:27 +0200

    Initial commit

--- src/components/Options.js
+++ src/components/Options.js
@@ -4,7 +4,7 @@ import Option from './Option';
 const Options = (props) => (
   <div>
     <div className="widget-header">
-      <h3>Your Options </h3>
+      <h3 className="widget-header__title">Your Options </h3>
       <button 
       className="button button--link"
       onClick={props.handleDeleteOptions}
@@ -12,10 +12,11 @@ const Options = (props) => (
       Remove All
       </button>
     </div>
-    {props.options.length === 0 && <p>Please add an option to get started!</p>}
+    {props.options.length === 0 && <p className="widget__message">Please add an option to get started!</p>}
     {
-      props.options.map((option) => (
+      props.options.map((option, index) => (
         <Option
+          count={index + 1}
           key={option}
           optionText={option}
           handleDeleteOption={props.handleDeleteOption}
