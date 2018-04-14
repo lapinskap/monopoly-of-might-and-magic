97626147d38e0d3316d4d68dbcd80d32e8ef3a75
Paulina <cherry3475@gmail.com>
2018-04-14 15:26:05 +0200

    Initial commit

---
+++ src/components/EditExpensePage.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const EditExpensePage = () => (
+    <div>This is my Edit Expense Page component</div>
+);
+
+export default EditExpensePage;
--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,37 +1,11 @@
 import {BrowserRouter, Route, Switch, Link, NavLink} from 'react-router-dom';
 import React from 'react';
-
-const ExpenseDashboardPage = () => (
-    <div>This is from my Dashboard component</div>
-);
-
-
-const AddExpensePage = () => (
-    <div>This is from my AddExpense component</div>
-);
-
-const HelpPage = () => (
-    <div>This is my Help Page component</div>
-);
-
-const EditExpensePage = () => (
-    <div>This is my Edit Expense Page component</div>
-);
-
-const NotFoundPage = () => (
-    <div>404
-    <br/> <Link to="/">go home u r drunk</Link> 
-    </div>
-);
-
-const Header = () => (
-<header>
-    <h1>Expensify</h1>
-    <NavLink to="/" activeClassName="is-active" exact={true}>Dashboard Page </NavLink>
-    <NavLink to="/create" activeClassName="is-active"> Create Expense</NavLink>
-    <NavLink to="/help"  activeClassName="is-active">Help Page</NavLink>
-</header>
-);
+import ExpenseDashboardPage from '../components/ExpenseDashboardPage';
+import AddExpensePage from '../components/AddExpensePage';
+import EditExpensePage from '../components/EditExpensePage';
+import Header from '../components/Header';
+import HelpPage from '../components/HelpPage';
+import NotFoundPage from '../components/NotFoundPage';
 
 const AppRouter = () => (
     <BrowserRouter>
