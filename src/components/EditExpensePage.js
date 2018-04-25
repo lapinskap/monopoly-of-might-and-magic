8429ab461c6987e6c124f352f7a40f4f1e76c6da
Paulina <cherry3475@gmail.com>
2018-04-25 17:56:13 +0200

    Initial commit

--- src/components/EditExpensePage.js
+++ src/components/EditExpensePage.js
@@ -1,7 +1,10 @@
 import React from 'react';
 
-const EditExpensePage = () => (
-    <div>This is my Edit Expense Page component</div>
-);
+const EditExpensePage = (props) => {
+    console.log(props);
+    return (
+        <div>Editing the expense with id of {props.match.params.id}</div>
+    );
+};
 
 export default EditExpensePage;
--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -15,7 +15,7 @@ const AppRouter = () => (
             <Route path="/" component={ExpenseDashboardPage} exact={true} />
             <Route path="/create" component={AddExpensePage}/>
             <Route path="/help" component={HelpPage}/>
-            <Route path="/edit" component={EditExpensePage}/>
+            <Route path="/edit/:id" component={EditExpensePage}/>
             <Route component={NotFoundPage}/>
 
         </Switch>
