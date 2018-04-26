f6487333867e380be8900479b797eb503a02eb2c
Paulina <cherry3475@gmail.com>
2018-04-26 18:05:42 +0200

    redux actions v1

--- src/playground/redux-101.js
+++ src/playground/redux-101.js
@@ -1,1 +1,25 @@
+import { createStore } from 'redux';
+//Actions in redux
+
 console.log('101');
+
+const store = createStore((state = { count: 0 }, action) => {
+   if(action.type === 'INCREMENT') {
+    return {
+        count: state.count + 1
+    }
+   } else {
+      return state; 
+   }
+   
+    console.log('running');
+    return state;
+});
+
+store.dispatch({
+    type: 'INCREMENT'
+});
+
+console.log(store.getState());
+
+
